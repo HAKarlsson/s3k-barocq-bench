2b1e1b7afc55f614683d6c5e00fc84ed86fc9a9e
Clément Chavanon <58697129+cchavano@users.noreply.github.com>
2026-05-08 10:32:42 +0200

    Update submodules

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ meson setup build/qemu/ipc ipc --optimization=$OPT --buildtype=release --cross-f
 # Setup Cheshire version
 PLATFORM=cheshire
 meson setup build/cheshire/hello hello --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM
-meson setup build/cheshire/ipc ipc --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM
+meson setup build/cheshire/ipc ipc --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM -Dtemporal_fence=false
 meson setup build/cheshire/ipc-cold ipc --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM -Dtemporal_fence=true
 meson setup build/cheshire/ipc-load ipc --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM -Dcounter='load'
 meson setup build/cheshire/ipc-store ipc --optimization=$OPT --buildtype=release --cross-file=$CROSS -Dplat=$PLATFORM -Dcounter='store'
